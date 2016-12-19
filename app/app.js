794e794bdba1410612fab55d17664851476992e2
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2016-12-19 17:54:42 -0500

    CORS preflight headers for x-access-token use

--- app/app.js
+++ app/app.js
@@ -12,7 +12,9 @@ const app = express()
 // Enable CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'x-access-token, X-Requested-With, Content-Type, Origin, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE')
+
   next()
 })
 
